310745f24dc516f9e66c3ec21b2682526547456f
apexualscar <narekhagopian@hotmail.com>
2025-08-13 17:14:45 +1000

    Add project files.

---
+++ ToolWindows/MyToolWindow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OllamaLocalHostIntergration
+{
+    public class MyToolWindow : BaseToolWindow<MyToolWindow>
+    {
+        public override string GetTitle(int toolWindowId) => "My Tool Window";
+
+        public override Type PaneType => typeof(Pane);
+
+        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+        }
+
+        [Guid("13ce5c07-1786-473f-8f88-cbb24110dec0")]
+        internal class Pane : ToolkitToolWindowPane
+        {
+            public Pane()
+            {
+                BitmapImageMoniker = KnownMonikers.ToolWindow;
+            }
+        }
+    }
+}
---
+++ ToolWindows/MyToolWindowControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OllamaLocalHostIntergration
+{
+    public partial class MyToolWindowControl : UserControl
+    {
+        public MyToolWindowControl()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            VS.MessageBox.Show("OllamaLocalHostIntergration", "Button clicked");
+        }
+    }
+}
